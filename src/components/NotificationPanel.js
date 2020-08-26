2a6aae4016e8a9fa0eb9161de9880e77428841e7
unknown <interstellarpf>
2020-08-26 03:10:03 +0200

    add this.state value

--- src/components/NotificationPanel.js
+++ src/components/NotificationPanel.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react'
+import { withAuth } from '../lib/Services/AuthProvider';
 
 class NotificationPanel extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            pendingInvitationList: this.props.user.pendingInvitation
 
         }
     }
+
+    
+
+
+
     
     render() {
+        console.log('PROPS', this.state.pendingInvitationList)
         return (
             <div>
-                
+                <ul>
+                    {}
+                </ul>
             </div>
+        
+            
+          
         )
     }
 }
 
-export default NotificationPanel
+export default withAuth(NotificationPanel)
